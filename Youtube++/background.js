eac60dd6fb03a986af6e015a031ceda83b9def4c
Tanush Sethi <tanushsethi55@gmail.com>
2022-08-30 02:57:07 +0530

    Youtube++: background scripts running

---
+++ Youtube++/background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    chrome.tabs.sendMessage(tabId, {
+      isYT: "true"
+    });
+  }
+});
