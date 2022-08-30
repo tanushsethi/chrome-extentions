dd8029666a42707b67287d6c164a8aa214b5a4f2
Tanush Sethi <tanushsethi55@gmail.com>
2022-08-30 18:32:55 +0530

    Youtube++: background scripts update

--- Youtube++/background.js
+++ Youtube++/background.js
@@ -1,7 +1,12 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    chrome.tabs.sendMessage(tabId, {
-      isYT: "true"
-    });
+    if (tab.url && tab.url.includes("youtube.com/watch")) {
+    chrome.tabs.sendMessage(tabId, "execute");
   }
 });
+
+
+function printLog(response){
+    console.log(response);
+}
+
+
--- Youtube++/contentScript.js
+++ Youtube++/contentScript.js
@@ -1,25 +1,18 @@
 (() => {
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
 
-    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.isYT == "true") {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            // To show/highlight the page action icon
-            chrome.pageAction.show(tabs[0].id);
-            changeUI();
-        });
-    }
-});
-
-    // chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    //     if (obj.type === "NEW") {
-    //         changeUI();
-    //     }
-    // });
+        if (obj === "execute") {
+        changeUI();
+   }
+    });
 
     const changeUI = () => {
         alert("running");
+        document.getElementById("player").remove();
     }
 
 })();
+        
 
-alert("running");
+
